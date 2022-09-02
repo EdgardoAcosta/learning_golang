992e366ec0dd988826d722cc89cd01decff3a785
EdgardoAcosta <acostaleale@gmail.com>
2022-09-02 12:14:07 -0500

    First commit Basic commands

---
+++ cards/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+
+func main() {
+
+	// var card string = "Ace of Spades"
+	cards := [] string{newCard(), newCard()}
+	cards = append(cards, "Six of Spades")
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+	// fmt.Println(cards)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
